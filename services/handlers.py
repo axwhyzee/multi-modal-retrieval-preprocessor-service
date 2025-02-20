a9d78190b82734cf5e87ffce00565e971ca9893c
Siah Wee Hung <34325457+axwhyzee@users.noreply.github.com>
2025-02-20 23:50:47 +0800

    use context manager for document

--- services/handlers.py
+++ services/handlers.py
@@ -13,19 +13,18 @@ repo = ObjectRepository()
 def handle_uploaded_doc(event: DocumentUploaded) -> None:
     doc_path = Path(event.document_path)
     doc_data = repo.get(event.document_path)
-    document = document_factory(doc_data, doc_path.suffix)
-
-    for i, obj in enumerate(document.generate_objs()):
-        obj_path = (
-            f'{doc_path.with_suffix("")}__{i}__{obj.obj_type}{obj.file_ext}'
-        )
-        repo.store(obj.data, f"META/{obj_path}")
-        publisher.publish(
-            ObjectPersisted(
-                document_path=str(doc_path),
-                object_path=str(obj_path),
+    with document_factory(doc_data, doc_path.suffix) as document:
+        for i, obj in enumerate(document.generate_objs()):
+            obj_path = (
+                f'{doc_path.with_suffix("")}__{i}__{obj.obj_type}{obj.file_ext}'
             )
-        )
+            repo.store(obj.data, f"META/{obj_path}")
+            publisher.publish(
+                ObjectPersisted(
+                    document_path=str(doc_path),
+                    object_path=str(obj_path),
+                )
+            )
 
 
 EVENT_HANDLERS: Dict[Type[Event], Callable] = {
