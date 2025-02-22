c214d07770bdb223e1ce266ba21cef3c56e44186
Siah Wee Hung <siah_weehung@yahoo.com>
2025-02-22 15:20:01 +0800

    use mapping service

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import logging
 from pathlib import Path
 
+from event_core.adapters.mapping import RedisMapper
 from event_core.adapters.pubsub import RedisConsumer
 from event_core.adapters.storage import add, get
 from event_core.domain.events import DocStored
 
 from domain.model import document_factory
+
+mapper = RedisMapper()
 
 
 def _handle_doc_callback(event: DocStored) -> None:
@@ -13,8 +16,12 @@ def _handle_doc_callback(event: DocStored) -> None:
     doc_data = get(event.obj_path)
     document = document_factory(doc_data, doc_path.suffix)
     for i, obj in enumerate(document.generate_objs()):
-        obj_path = f'{doc_path.with_suffix("")}__{i}__{obj.type}{obj.file_ext}'
+        obj_path = doc_path.with_suffix("")
+        obj_path += f"__{i}"
+        obj_path += f"__{obj.type}"
+        obj_path += obj.file_ext
         add(obj.data, f"META/{obj_path}", obj.type)
+        mapper.set(obj_path, doc_path)  # obj -> doc (parent)
 
 
 def main():
