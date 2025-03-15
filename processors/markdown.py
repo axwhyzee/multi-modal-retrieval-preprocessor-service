beb89a8271931168bf3013b47b8458c728a88144
Siah Wee Hung <siah_weehung@yahoo.com>
2025-03-16 00:03:54 +0800

    fix mypy

--- processors/markdown.py
+++ processors/markdown.py
@@ -4,8 +4,8 @@ from event_core.domain.types import Element, FileExt
 
 from processors.base import AbstractProcessor
 from processors.code import CodeProcessor
-from processors.text import TextProcessor
 from processors.common import Unit
+from processors.text import TextProcessor
 
 
 class MarkdownProcessor(AbstractProcessor):
@@ -18,19 +18,22 @@ class MarkdownProcessor(AbstractProcessor):
         blocks = self._data.decode("utf-8").split("```")
         seq = 0
         for i, block in enumerate(blocks):
-            block = block.strip().encode("utf-8")
+            block = block.strip()
             if not block:
                 continue
 
+            enc_block = block.encode("utf-8")
             if i % 2:
                 # code block
-                for unit in CodeProcessor(block, FileExt.PY)():  # any file ext will do
+                for unit in CodeProcessor(
+                    enc_block, FileExt.PY
+                )():  # any file ext will do
                     unit.seq = (seq := seq + 1)
                     unit.type = Element.CODE
                     yield unit
             else:
                 # text block
-                for unit in TextProcessor(block)():
+                for unit in TextProcessor(enc_block)():
                     unit.seq = (seq := seq + 1)
                     unit.type = Element.TEXT
                     yield unit
